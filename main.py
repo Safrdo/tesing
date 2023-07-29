f5e9bc7174ae2f4e206b3a1eca2105e4b641f267
Safrdo <82527752+Safrdo@users.noreply.github.com>
2023-07-29 14:53:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -74,9 +74,10 @@ def create_order(api_key, secret_key, coin_pair, position, buy_leverage, percent
     return response.json()
 
 
+
 @app.route('/', methods=['POST'])
 def webhook():
-     try:
+    try:
         data = request.get_json()
 
         # Získání hodnot z TradingView alert message
